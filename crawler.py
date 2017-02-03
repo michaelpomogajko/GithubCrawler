e693ee386aa51cf6352e74dd54570ef9554e5d29
Michael Pomogajko <m.pomogajko@gmail.com>
2017-02-03 12:57:28 +0100

    minor

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ import time
 import database
 
 '''
-Github unsecure API crawler
+Github unsecured API crawler
 @author Misha Frenkman
 @version 0.1
 
@@ -22,7 +22,7 @@ def checkReset():
 	
 def crawler():
 	startID = 0
-	lastID = 0
+	
 	while True:	
 		startID = database.checkLastID() or 0
 		req = githubURL+str(startID)
@@ -30,12 +30,12 @@ def crawler():
 		
 		#Check for the right Github response
 		if type(res) is list:
-			# Check for private
+			#Iterate Repos
 			for repo in res:
+				# Check for private
 				if repo[u'private'] == False:
+					#Insert Repo in Database
 					database.insertRepo(repo[u'id'], repo[u'full_name'], repo[u'html_url'])
-					
-			lastID = res[-1][u'id']
 		else:
 			print "Github kicked me out :( Rate limit reset in: %i minutes" % (checkReset())
 			print "Timestamp: %s" % (time.strftime("%d.%m.%Y %H:%M:%S"))
