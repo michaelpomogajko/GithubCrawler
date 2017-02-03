1231c069f0b814dc1c163cdcc526a74fc198fb4f
Michael Pomogajko <m.pomogajko@gmail.com>
2017-02-03 11:42:50 +0100

    Check for Limit

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ import sys
 import json
 import requests
 import time
+import datetime 
 from database import *
 
 '''
@@ -16,17 +17,22 @@ Github unsecure API crawler
 
 githubURL = "https://api.github.com/repositories?since="
 
-
+def checkReset():
+	url = "https://api.github.com/rate_limit"
+	resetDate = requests.get(url).json()[u'rate'][u'reset']
+	return int(round((resetDate - time.time()) / 60))+1
+	
 def crawler():
 	startID = 0
 	lastID = 0
 	while True:	
-		startID = checkLastID()
+		startID = checkLastID() or 0
 		req = githubURL+str(startID)
 		res = requests.get(req).json()
 		
 		#Check for the right Github response
 		if type(res) is list:
+			print "-- Crawling for Repos -- "
 			# Check for private
 			for repo in res:
 				if repo[u'private'] == False:
@@ -34,9 +40,10 @@ def crawler():
 					
 			lastID = res[-1][u'id']
 		else:
-			print "Github kicked me out :( waiting 1min to reconnect :("
-			print "Timestamp: %s" % (time.strftime("%d.%m.%Y %H:%M"))
-			time.sleep(60)
+			print "Github kicked me out :( Limit Rate Reset in: %i minutes" % (checkReset())
+			print "Timestamp: %s" % (time.strftime("%d.%m.%Y %H:%M:%S"))
+			
+			time.sleep(60*10)
 
 crawler()
 	
